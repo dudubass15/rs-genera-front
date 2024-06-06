d270c33fe94229d93ab5b2a3f4652c6b49d82c98
Carlos eduardo <carlos.contato00@gmail.com>
2024-06-05 23:30:42 -0300

    Adjusts environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000/api/v1',
+    // baseUrl: 'http://localhost:3000/api/v1',
+    baseUrl: 'https://memoriasafetos-api.carloseduardo.dev/api/v1',
 };
